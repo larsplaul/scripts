9aadb4edfe320334a445385f0309f5f4516bce02
Plaul <larsplaul@gmail.com>
2014-02-04 22:00:54 +0100

    Scripts for the first two days

---
+++ dayOneHotelDemo.sql
@@ -0,0 +1,34 @@
+drop table ROOM;
+drop table HOTEL;
+
+CREATE TABLE HOTEL (
+  hotelNo number primary key,
+  hotelName varchar2(40),
+  city varchar2(35)
+);
+
+
+CREATE TABLE ROOM (
+roomNo number ,
+hotelNo number not null,
+type varchar2(10) default 'double' check(type in ('single','double','suite')),
+price number NULL ,
+PRIMARY KEY (roomNo,hotelNo),
+FOREIGN KEY( hotelNo) REFERENCES HOTEL(hotelNo)
+);
+
+insert into HOTEL values(1,'SAS','London');
+insert into HOTEL values(2,'Hilton','Oslo');
+insert into HOTEL values(3,'Grand','Paris');
+insert into HOTEL values(4,'Palads','Odense');
+
+insert into ROOM (ROOMNO,HOTELNO,PRICE) VALUES (100,1,600);
+insert into ROOM VALUES (200,1,'double',600);
+insert into ROOM VALUES (300,1,'single',500);
+insert into ROOM(ROOMNO,HOTELNO) VALUES (100,2);
+insert into ROOM VALUES (200,2,'suite',1200);
+insert into ROOM VALUES (200,3,'double',900);
+
+
+
+
